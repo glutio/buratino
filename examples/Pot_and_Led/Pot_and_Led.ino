6433748f3a44ea9b903eb1aedfa854d121354ad8
Eugene Pistrak <evpistra@microsoft.com>
2023-05-16 06:58:12 -0700

    add BSync

--- examples/Pot_and_Led/Pot_and_Led.ino
+++ examples/Pot_and_Led/Pot_and_Led.ino
@@ -3,17 +3,13 @@
 BDigitalPin led(6, BPinMode::Output, BPinTrigger::Never);
 BAnalogPin pot(0 /* pin */);
 
-int16_t timeout = 1000;
+BSync<uint16_t> timeout(1000);
+
 void TimerTask(Buratino* a, void* b) {  
   auto start = millis();
   while (1) {
     // disable task switching while reading shared global variable into local variable
-    auto sreg = SREG; // save state register (where interrupts flag is)
-    noInterrupts(); // disable interrupts (clear the flag)
-    auto local = timeout; // save global value locally
-    SREG = sreg; // return to whatever state interrupt flag was before
-
-    if (millis() - start > local)
+    if (timeout < (uint16_t)(millis() - start))
     {
       led(!led);
       start = millis();
@@ -23,17 +19,16 @@ void TimerTask(Buratino* a, void* b) {
 }
 
 void OnPotChange(BAnalogPin* pin, BAnalogPinChangeArgs* args) {
-  auto sreg = SREG;
-  noInterrupts();
   timeout = args->value;
-  SREG = sreg;
 }
 
 void setup() {
   pot.Reset();
   pot.OnChange = BAnalogPin::ChangeEvent(OnPotChange);
-  noInterrupts(); // disable interrupts (while setting up tasks)
+  
+  Buratino::Stop();
   Buratino::Setup(1 /* number of tasks */); // sets up time switcher interrupt
+  Buratino::Start();
   Buratino::RunTask(BTask(TimerTask), 0, 1024);
   interrupts(); // enable task switcher
 }
